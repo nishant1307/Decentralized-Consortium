6777f7393bbe129402bd691a441249fcd0cc979b
Nishant <nishant.mitra94@gmail.com>
2019-09-18 13:14:15 +0530

    Storage Def changes

--- contracts/StorageDefinition.sol
+++ contracts/StorageDefinition.sol
@@ -2,11 +2,9 @@ pragma solidity ^0.5.11;
 contract StorageDefinition {
     struct Storage {
         mapping(bytes32 => bool) _bool;
-        mapping(bytes32 => int) _int;
         mapping(bytes32 => uint256) _uint;
         mapping(bytes32 => string) _string;
         mapping(bytes32 => address) _address;
-        mapping(bytes32 => bytes32) _bytes32;
     }
 
     struct User {
@@ -16,6 +14,7 @@ contract StorageDefinition {
         KYCStatus status;
         string kycHash;
         roles role;
+        bool adminApprovalStatus;
     }
     struct Organization {
         string organizationID;
@@ -28,8 +27,6 @@ contract StorageDefinition {
 
     enum KYCStatus { kycPending, kycComplete, suspended }
 
-    enum partnerRoles { buyer, seller, logistics, agent, bank }
-
     enum projectItems { document, device, product }
 
     struct Project {
@@ -37,20 +34,22 @@ contract StorageDefinition {
         string name;
         string description;
         string industry;
+        address projectAdmin;
     }
 
     struct PartnershipRequest {
+        string partnershipRequestID;
         string partnershipType;
         string organizationID;
         string partnershipDoc;
         KYCStatus partnershipStatus;
     }
 
-    event ProjectCreated(bytes32 indexed _projectID, string name, address _by, uint256 timestamp);
-    event PartnerAddedToConsortium(bytes32 indexed _projectID, address _by, address userAddress, partnerRoles partnerRole, uint256 timestamp);
-    event DocumentAdded(bytes32 indexed _projectID, address _by, string itemID, uint256 timestamp);
-    event DeviceAdded(bytes32 indexed _projectID, address _by, string itemID, uint256 timestamp);
-    event ProductAdded(bytes32 indexed _projectID, address _by, string itemID, uint256 timestamp);
+    event ProjectCreated(bytes32 indexed _projectID, string name, string _by, uint256 timestamp);
+    event PartnerAddedToConsortium(bytes32 indexed _projectID, string _by, string partnerOrganization, string partnerRole, uint256 timestamp);
+    event DocumentAdded(bytes32 indexed _projectID, string _by, string itemID, uint256 timestamp);
+    event DeviceAdded(bytes32 indexed _projectID, string _by, string itemID, uint256 timestamp);
+    event ProductAdded(bytes32 indexed _projectID, string _by, string itemID, uint256 timestamp);
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event ContractRegistered(address indexed contractAddress, string contractName, uint256 timestamp);
